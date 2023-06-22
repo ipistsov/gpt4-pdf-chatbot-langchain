cda804a3ef258b22c07b71ca614b3763833e86ab
ipistsov <40342645+ipistsov@users.noreply.github.com>
2023-06-22 16:31:16 -0400

    API key values updated

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,8 +9,9 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+const QA_PROMPT = `You are a helpful AI Paralegal, specializing on immigration law. Use the following pieces of context to answer the question at the end.
+If the question does not have a clear answer, request more information from the user. If additional information does not help, suggest to discuss the matter 
+with an immigration lawyer. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
@@ -21,7 +22,7 @@ Helpful answer in markdown:`;
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    modelName: 'gpt-3.5-turbo-16k-0613', //change this to gpt-4 if you have access
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
