889f2b80b018bab8524acdd7e46bd8efa847692e
ipistsov <40342645+ipistsov@users.noreply.github.com>
2023-06-23 16:20:39 -0400

    embeddings by 4000 tokens

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -24,8 +24,8 @@ export const run = async () => {
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
+      chunkSize: 4000,    // max number of tokens per chunk
+      chunkOverlap: 400,
     });
 
     const docs = await textSplitter.splitDocuments(rawDocs);
