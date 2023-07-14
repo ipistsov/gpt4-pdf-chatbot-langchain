4f63041f083a2b89dc22dc01382a35d6cf6aa47a
Rana Haris Ali <ranaharisali42@yahoo.com>
2023-07-14 18:27:55 +0500

    add comments

--- pages/api/chat/message.ts
+++ pages/api/chat/message.ts
@@ -15,7 +15,8 @@ export default async function handler(
 		return res.status(405).json({ error: 'Method not allowed' });
 	}
 
-	const [ hash, question ] = req.body;
+	// extract hash and question from the request body
+	const [hash, question] = req.body;
 
 	if (!question || !hash) {
 		return res.status(400).json({ message: 'No question or hash in the request' });
@@ -25,12 +26,13 @@ export default async function handler(
 	const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
 
 	try {
+		// get the last 10 questions of the user with this hash
 		let history = await excuteQuery({
 			query: 'SELECT * FROM history WHERE hash = ? ORDER BY created_at DESC LIMIT 10',
 			values: [hash]
 		}) as any[];
 
-		// if the response is still pending return empty response
+		// if the question is same and response is still pending return empty response
 		if (history[0]?.question === sanitizedQuestion) {
 			if (history[0]?.pending) {
 				return res.status(200).json({
@@ -40,6 +42,7 @@ export default async function handler(
 					sourceData: [],
 				});
 			} else {
+				// if the response is not pending, return the answer
 				return res.status(200).json({
 					success: true,
 					data: history[0].answer ?? "",
@@ -49,8 +52,12 @@ export default async function handler(
 			}
 		}
 
-		history = history.map(record => record.question)
+		// get the question history of current user for context
+		history = history.reverse().map(record => {
+			return [record.question, record.answer]
+		})
 
+		// insert the question in database
 		const insertedRecord = await excuteQuery({
 			query: 'INSERT INTO history(hash, question) VALUES(?, ?);',
 			values: [hash, sanitizedQuestion]
@@ -76,6 +83,7 @@ export default async function handler(
 				});
 			})
 			.then(response => {
+				// sanitize the response according to our structure
 				const sourceData = []
 				for (let index = 0; index < response.sourceDocuments.length; index++) {
 					if (index >= 3) {
@@ -93,6 +101,7 @@ export default async function handler(
 				}
 			})
 			.then(async sanitizedResponse => {
+				// save the response in database
 				await excuteQuery({
 					query: 'UPDATE history SET pending = 0, answer = ?, source_data = ? WHERE id = ?;',
 					values: [
@@ -103,6 +112,7 @@ export default async function handler(
 			})
 			.catch(console.error);
 
+		// return empty response after OpenAI API has been triggered
 		return res.status(200).json({
 			success: true,
 			data: "",
