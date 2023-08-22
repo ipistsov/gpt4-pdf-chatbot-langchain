5ce625e29f3c9bc18a93c7c60b2de2181fd62016
Rana Haris Ali <ranaharisali42@yahoo.com>
2023-08-23 01:19:54 +0500

    improve followup questions prompt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -14,7 +14,7 @@ Use the following pieces of context to answer the question at the end.
 If the question does not have a clear answer, suggest trying to rephrase the question or to discuss the matter with an immigration lawyer. 
 DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the immigration law.
-Also, in case the primary question is related to immigration, please suggest two best follow-up questions - have them listed at the end of the response in the format: "!QUESTIONS!: [Question 1, Question 2]"
+Also, in case the primary question is related to immigration, please suggest two best follow-up questions that the user can ask from you - have them listed at the end of the response in the format: "!QUESTIONS!: [Question 1, Question 2]"
 
 {context}
 
