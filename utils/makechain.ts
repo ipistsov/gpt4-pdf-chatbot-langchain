37e029696f3597f3b3afe6c9fc9ef87f81621315
Rana Haris Ali <ranaharisali42@yahoo.com>
2023-08-31 01:01:36 +0500

    change model to gpt-3.5-turbo

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -25,7 +25,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
     temperature: 0, // increase temepreature to get more creative answers
     // modelName: 'gpt-3.5-turbo-16k-0613', //change this to gpt-4 if you have access
-    modelName: 'gpt-4-0613',
+    modelName: 'gpt-3.5-turbo',
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
