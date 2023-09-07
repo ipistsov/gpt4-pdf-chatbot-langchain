7d1fd0a9ac9b2f220d68439f82ad7448e8de405d
Rana Haris Ali <ranaharisali42@yahoo.com>
2023-09-08 01:10:55 +0500

    add script for deleting a namespace

---
+++ utils/deletePineconeVectors.ts
@@ -0,0 +1,10 @@
+import { pinecone } from '@/utils/pinecone-client';
+
+// Code has been commented to make sure this script is not run accidentally
+
+// const index = pinecone.Index(process.env.PINECONE_INDEX_NAME ?? '');
+// await index.delete1({
+//   deleteAll: true,
+//   namespace: "AFFIRMATIVE-ASYLUM-PROCEDURES-MANUAL-attempt-2"
+// });
+
