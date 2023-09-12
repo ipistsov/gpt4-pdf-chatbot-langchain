2110416b62bd3964b9d6df010ea6f7431ebd9cc6
Rana Haris Ali <ranaharisali42@yahoo.com>
2023-09-13 04:31:26 +0500

    add prompt for handling colloquial questions

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -18,7 +18,7 @@ const generatePrompts = (client: string) => {
 
 			QA_PROMPT = `You are a helpful AI Paralegal, named Exsy. You are an expert in immigration law. 
 			Use the following pieces of context to answer the question at the end.
-			If the question does not have a clear answer, suggest trying to rephrase the question or to discuss the matter with an immigration lawyer. 
+			If the question does not have a clear answer, or if the question is asked in a colloquial language, respond with a question suggestion in "legal jargon" relevant to the question that has been asked, or give suggestion to discuss the matter with an immigration lawyer. 
 			DO NOT try to make up an answer.
 			If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the immigration law.
 			Also, in case the primary question is related to immigration, please suggest two best follow-up questions - have them listed at the end of the response in the format: "!QUESTIONS!: \nQuestion 1 \nQuestion 2"
